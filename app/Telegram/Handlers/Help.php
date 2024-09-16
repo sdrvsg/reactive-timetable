693221f6f186eb9d9a6086021a48b57c9c7cde2f
Оникс <onyxservers@yandex.ru>
2024-09-16 17:29:29 +0400

    A whole commit!

--- app/Telegram/Handlers/Help.php
+++ app/Telegram/Handlers/Help.php
@@ -18,6 +18,6 @@ class Help
 
         }
 
-        $bot->asResponse()->sendImagedMessage("<b>Реактивное расписание? Да ну</b>\n\nКороче меньше пиздежа, команды такие:\n/start — Данное окошко\n/timetable — Расписание\n/group — Сменить группу\n/transfer — Передать старосту\n\n<b>Для групп только <i>Инлайн</i> режим:</b>\n\n<code>@ssauReactiveBot {сегодня, завтра, 11 сен, 15.12.2024...}</code>\n<i>Скинуть расписание указанной даты</i>");
+        $bot->asResponse()->sendImagedMessage("<b>Реактивное расписание? Да ну</b>\n\nКороче меньше пиздежа, команды такие:\n/start — Данное окошко\n/timetable — Расписание\n/group — Сменить группу\n/transfer — Передать старосту\n\n<b>Для групп только <i>Инлайн</i> режим:</b>\n\n<code>@ssauReactiveBot {сегодня, завтра, 11 сен, 15.12.2024...}</code>\n<i>Скинуть расписание указанной даты</i>\n\n<blockquote>Бот находится в состоянии beta-теста, просьба баги и предложения кидать на <a href='https://github.com/sdrvsg/reactive-timetable/issues'>GitHub</a>\nНа код смотреть не стоит. Это было давно и не правда</blockquote>");
     }
 }
